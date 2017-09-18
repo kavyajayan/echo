e1a401dff5eea3200e681c4ad54c1ee21e754174
kavyajayan <kavyakjayachandran.mec@gmail.com>
2017-09-18 18:24:21 +0530

    added all the files

--- echo.component.ts
+++ echo.component.ts
@@ -7,22 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EchoComponent implements OnInit {
 
+  text: string="";
   constructor() { }
+
+  newCommand: string = "";
 
   ngOnInit() {
   }
-  commands = [];
-  addCommand(newCommand: string) {
-    if (newCommand) {
-      this.commands.push("user@ubuntu:$"+newCommand);
+  commands=[];
+  addCommand() {
+    var obj = new CommandTerminal();
+    if (this.newCommand) {
+      obj.usercommand=this.newCommand;
+      obj.output="loading...";
+      this.commands.push(obj);
+      var commandjson = {command : obj.usercommand};
+      /*http
+        .post('endpoint',commandjson);
+        .subscribe(){
+          console.log(data);
+          /*obj=this.commands[this.commands.length-1];
+          obj.output="output";
+        }*/
+        this.newCommand = "";
     }
+  }
+  pressSubmit(){
+    /*if(this.text){
+      var codejson = {string : this.text};
+        http
+          .post('endpointsubmit', codejson);
+          .subscribe(){
+          }
+    }*/
   }
 }
-/*export class Prompt{
-  commands = [];
-  addCommand(newCommand: string) {
-    if (newCommand) {
-      this.commands.push(newCommand);
-    }
-  }
-}*/
+class CommandTerminal{
+  usercommand: string;
+  output : string;
+}
